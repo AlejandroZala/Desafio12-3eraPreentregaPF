11daf0668706131b4eec82f6aff3cfc5123f074e
AlejandroZala <galeza2012@hotmail.com>
2023-07-19 19:20:39 -0300

    services.js

--- src/services/services.js
+++ src/services/services.js
@@ -4,16 +4,16 @@ import UsersService from "./user.service.js";
 export const userService = new UsersService(new UserManager());
 
 import ProductsManager from '../dao/managers/productsManager.js'
-import ProductsServices from '../services/productsService.js'
+import ProductsServices from './product.service.js';
 
 export const productsService= new ProductsServices(new ProductsManager)
 
 import CartsManager from '../dao/managers/cartManager.js'
-import CartsService from './cartService.js'
+import CartsService from './cart.service.js';
 
 export const cartsService= new CartsService(new CartsManager)
 
 import TiketManager from '../dao/managers/tiketManager.js'
-import TiketServices from '../services/tikets.service.js'
+import TiketServices from './tickets.service.js';
 
 export const tiketService= new TiketServices(new TiketManager)
